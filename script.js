31e02106cb821e1cd098b0f544762efbe3a3f2dd
Wesley Jr <wesley_damazio@hotmail.com>
2023-06-21 21:19:00 -0300

    .

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-const addItemBtn = document.querySelector('#addItem');
+// evento adicionar item
+const addBtn = document.querySelector("#add-btn");
 
-addItemBtn.addEventListener("click", function(e){
+addBtn.addEventListener("click", function(e){
+
     e.preventDefault();
-    addItem();
-    console.log(textoItem)
+    
 });
-
-function addItem(){
-    const textoItem = document.querySelector('#textoItem').value;
-
-    if(textoItem){
-    const item = document.querySelector('.item')
-    const novoItem = item.cloneNode(true)
-    const lista = document.querySelector('#containerLista');
-    lista.appendChild(novoItem);
-    }
-}
